5d42ccead2f87137329f21845cefdf6a377e1b90
Fuast Pan <xpan2003@gmail.com>
2025-08-20 22:39:05 -0500

    finish of phase 1. a interesting experience of lora. what I did is Load a base LLM, attach Lora, run finetune loop and save and reload the adapter

--- scripts/script.py
+++ scripts/script.py
@@ -2,3 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 model_name = "NousResearch/Llama-2-7b-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+
+
+from datasets import load_dataset
+ds = load_dataset("tatsu-lab/alpaca")
